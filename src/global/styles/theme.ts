9d0a737aff6c2a9492a924f34a81db5f4897b1f3
Vinicius Italo <vinicius.italo@fretebras.com.br>
2022-12-18 16:04:12 -0300

    feat: create global theme for application

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,28 @@
+export const THEME_DEFAULT = {
+	colors: {
+		brand: '#1BA665',
+		primary: '#F2B441',
+		secondary: '#D94D1A',
+		text: '#212121',
+		shape: '#F2F2F2',
+		white: '#FFFFFF',
+	},
+	fonts: {
+		size: {
+			sm: 0.75,
+			md: 0.875,
+			rg: 1,
+			lg: 1.5,
+			xlg: 2,
+		},
+		weight: {
+			light: 300,
+			regular: 400,
+			bold: 700,
+		},
+		family: {
+			title: 'Playfair Display',
+			text: 'Poppins',
+		},
+	},
+};
