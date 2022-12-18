2c0ec73ec3f83dc78ba0877f426ca5bf073f3540
Vinicius Italo <vinicius.italo@fretebras.com.br>
2022-12-18 16:54:34 -0300

    test: verify description in footer

---
+++ __tests__/components/Footer.spec.tsx
@@ -0,0 +1,21 @@
+import { render } from '@testing-library/react';
+import { ThemeProvider } from 'styled-components';
+
+import { Footer } from '../../src/global/components/Footer';
+import { THEME_DEFAULT } from '../../src/global/styles/theme';
+
+describe('Home', () => {
+	it('Should renders descriptions', () => {
+		const { getByText } = render(
+			<ThemeProvider theme={THEME_DEFAULT}>
+				<Footer />
+			</ThemeProvider>
+		);
+		const description =
+			'Desenvolvido por Vinicius Italo para aperfeiçoamento de técnologias da programação front-end';
+		const copyright = 'Desenvolvido em 2022';
+
+		expect(getByText(description)).toBeInTheDocument();
+		expect(getByText(copyright)).toBeInTheDocument();
+	});
+});
