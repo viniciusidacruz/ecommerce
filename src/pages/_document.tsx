a1cf1f9091f873526b569490a638d89f0f919b7e
Vinicius Italo <vinicius.italo@fretebras.com.br>
2022-12-18 13:24:09 -0300

    chore: implement eslint, prettier and editorconfig for code patterns

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,20 +1,20 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-  return (
-    <Html lang="pt-BR">
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,700;1,400&family=Poppins:wght@300;400;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+	return (
+		<Html lang="pt-BR">
+			<Head>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" />
+				<link
+					href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,700;1,400&family=Poppins:wght@300;400;700&display=swap"
+					rel="stylesheet"
+				/>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,14 @@ import Head from 'next/head';
 import { Fragment } from 'react';
 
 export default function Home() {
-  return (
-    <Fragment>
-      <Head>
-        <title>First Page</title>
-      </Head>
-      <main>
-        <h1>Hello World</h1>
-      </main>
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<Head>
+				<title>First Page</title>
+			</Head>
+			<main>
+				<h1>Hello World</h1>
+			</main>
+		</Fragment>
+	);
 }
