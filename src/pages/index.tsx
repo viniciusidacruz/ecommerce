4b14ae2fe2121f4e9a9b06a58b6572a020efe017
Vinicius Italo <vinicius.italo@fretebras.com.br>
2022-12-19 22:54:51 -0300

    chore: implement redux and configuring

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,7 @@
 import Head from 'next/head';
 import { Fragment } from 'react';
-import { useAppDispatch, useAppSelector } from '../global/hooks/redux';
-import { incrementByAmount, selectCount } from '../global/store/counterSlice';
 
 export default function Home() {
-	const dispatch = useAppDispatch();
-	const count = useAppSelector(selectCount);
-
-	console.log('Count: ', count);
-
 	return (
 		<Fragment>
 			<Head>
@@ -16,13 +9,6 @@ export default function Home() {
 			</Head>
 			<main>
 				<h1>Hello World</h1>
-
-				<button
-					type="button"
-					onClick={() => dispatch(incrementByAmount(count + 1))}
-				>
-					Incrementar
-				</button>
 			</main>
 		</Fragment>
 	);
