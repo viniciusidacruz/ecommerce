d53415c96157cdee55cfb7bc27df52b08b7fee52
Vinicius Italo <vinicius.italo@fretebras.com.br>
2022-12-19 22:53:28 -0300

    chore: implement redux and configuring

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,8 @@
 import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
+
+import { store } from '../global/store';
 
 import { Footer, Header } from '../global/components';
 
@@ -9,10 +12,12 @@ import { THEME_DEFAULT } from '../global/styles/theme';
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<ThemeProvider theme={THEME_DEFAULT}>
-			<ResetCSS />
-			<Header />
-			<Component {...pageProps} />
-			<Footer />
+			<Provider store={store}>
+				<ResetCSS />
+				<Header />
+				<Component {...pageProps} />
+				<Footer />
+			</Provider>
 		</ThemeProvider>
 	);
 }
