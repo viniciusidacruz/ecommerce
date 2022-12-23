6ef53f5a1d0fb099558deb209ef07fec6967ce7f
Vinicius Italo <vinicius.italo@fretebras.com.br>
2022-12-23 00:36:20 -0300

    feat: implement routing for login and configuring test in Header

--- __tests__/components/Header.spec.tsx
+++ __tests__/components/Header.spec.tsx
@@ -4,6 +4,10 @@ import { ThemeProvider } from 'styled-components';
 import { Header } from '../../src/global/components';
 import { THEME_DEFAULT } from '../../src/global/styles/theme';
 
+jest.mock('next/router', () => ({
+	useRouter: jest.fn(),
+}));
+
 describe('Header', () => {
 	it('Should renders', () => {
 		const { getByText } = render(
--- src/global/components/Header/index.tsx
+++ src/global/components/Header/index.tsx
@@ -1,16 +1,25 @@
-import { Container } from '../../styles/resetCSS';
+import { useRouter } from 'next/router';
+
 import { Button } from '../Button';
 import { Drawer } from '../Drawer';
 import { Logo } from '../Logo';
+
+import { Container } from '../../styles/resetCSS';
 import * as Styles from './styles';
 
 export function Header() {
+	const router = useRouter();
+
 	return (
 		<Styles.Wrapper>
 			<Container>
 				<Logo />
 
-				<Button title="Entrar" variant="secondary" />
+				<Button
+					title="Entrar"
+					variant="secondary"
+					onClick={() => router.push('/entrar')}
+				/>
 
 				<Drawer />
 			</Container>
